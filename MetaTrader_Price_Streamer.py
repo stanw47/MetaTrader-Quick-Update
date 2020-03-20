223aef2155c720856bb293c9ef1ec3b4ef098372
Stanley Williamson <stanleywilliamson47@gmail.com>
2020-03-20 10:38:13 -0400

    created loop so that it will stream, added question

--- MetaTrader_Price_Streamer.py
+++ MetaTrader_Price_Streamer.py
@@ -1,5 +1,5 @@
+#needs to use metatrader5 for chart data and datetime for the current time
 from datetime import datetime
-
 import MetaTrader5 as mt5
  
 
@@ -11,8 +11,16 @@ if not mt5.initialize():
 print(mt5.terminal_info())
 print(mt5.version())
 
-gbpusd_ticks = mt5.copy_ticks_from("GBPUSD", datetime.now(), 5000, mt5.COPY_TICKS_ALL)
+gbpusd_ticks = mt5.copy_ticks_from("GBPUSD", datetime.now(), 1, mt5.COPY_TICKS_ALL)
+
+now = datetime.now()
 
 print('gbpusd_ticks(', len(gbpusd_ticks), ')')
 for val in gbpusd_ticks[:5000]: print(val)
 
+start = input("Would you now like to stream the price of GBPUSD? (y/n): ")
+if start == "y":
+    count = 0
+    while (count < 1):   
+      print(gbpusd_ticks, now)
+      
